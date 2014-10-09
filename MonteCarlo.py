2615f91f6078bf68056f85b133a5f0177feb3c9f
smithy545 <smithy545@gmail.com>
2014-10-09 11:59:00 -0400

    Converted Monte Carlo file into function format

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -13,52 +13,43 @@ import LinearBestFitFunction as fit
 #    import site as fit
 #    site.addsitedir("https://raw.githubusercontent.com/Case-Research-P-N-S/Band-Optimizing-for-Foreground-Subtraction/master/LinearBestFitFunction.py")
 
+def monteCarloGen(xList, aTrue, bTrue, std):   
+  
+    # ---------------------------------------------------
+    # Theoretical Values for a and b (slope and intercept)
 
-# ---------------------------------------------
-# Finding a standard deviation value for the Monte Carlo 
-# it's probably a function. don't know yet
-std = 1
-
-
-# ---------------------------------------------------
-# Theoretical Values for a and b (slope and intercept)
-
-aTrue = 1
-bTrue = 1
-
-aList = []
-bList = []
-rSquareList = []
-
-# ------------------------------------------------
-# Creating Best Fit Line, storing, and reiterating.
-
-
-xList = [1,2,3,4,5]
-
-
-# assuming x's are constant, may later generate x's from a normal distribution
-# how many times the Monte Carlo is repeated
-iterations = 1000
-for i in range(iterations):
-    # generating yList
-    yList = [aTrue + bTrue*x + np.random.normal(0, std) for x in xList]
-    # applying lines of best fit
-    results = fit.linearFit(xList, yList)
-    # creating an aList, bList, and rSquareList for later analysis
-    aList.append(results[0])
-    bList.append(results[1])
-    rSquareList.append(results[2])
-
-histoDataA = np.histogram(aList)
-histoDataB = np.histogram(bList)
-
-
-plt.figure(0)
-plt.plot(aList, bList, 'bo')
-
-plt.figure(1)
-plt.bar(histoDataB[1][0:-1], histoDataB[0])
-
-plt.figure(2)
-plt.bar(histoDataA[1][0:-1], histoDataA[0])
+    aList = []
+    bList = []
+    rSquareList = []
+    
+    # ------------------------------------------------
+    # Creating Best Fit Line, storing, and reiterating.
+    
+    # assuming x's are constant, may later generate x's from a normal distribution
+    # how many times the Monte Carlo is repeated
+    iterations = 1000
+    for i in range(iterations):
+        # generating yList
+        yList = [aTrue + bTrue*x + np.random.normal(0, std) for x in xList]
+        # applying lines of best fit
+        results = fit.linearFit(xList, yList)
+        # creating an aList, bList, and rSquareList for later analysis
+        aList.append(results[0])
+        bList.append(results[1])
+        rSquareList.append(results[2])
+    
+    histoDataA = np.histogram(aList)
+    histoDataB = np.histogram(bList)
+    
+    
+    plt.figure(0)
+    plotFit = fit.linearFit(aList, bList)
+    plotList = [x for x in np.arange(min(aList), max(aList), 0.1)]
+    plt.plot(aList, bList, 'bo')
+    plt.plot(plotList, [plotFit[0] + plotFit[1]*x for x in plotList])
+    
+    plt.figure(1)
+    plt.bar(histoDataB[1][0:-1], histoDataB[0])
+    
+    plt.figure(2)
+    plt.bar(histoDataA[1][0:-1], histoDataA[0])
